0d33317014f29f088955ffa7b853b28e1eda2366
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-11-03 23:06:04 -0400

    Add shell interop

--- src/pho_diff/core.clj
+++ src/pho_diff/core.clj
@@ -1,8 +1,16 @@
 (ns pho-diff.core
   (:gen-class)
-  (:require [clojure.spec.alpha :as spec]))
+  (:require [clojure.spec.alpha :as spec]
+            [clojure.java.shell :as shell]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def english-cons "resources/inventory")
+
+(shell/sh "ls" english-cons)
+
+
+(shell/sh "convert")
