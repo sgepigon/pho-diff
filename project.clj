a407033b44050db0f7a49f3da0d3c80f42b58ea5
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-11-05 23:18:01 -0500

    Add expound
    Helps with the clojure.spec output

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-beta4"]
-                 [me.raynes/conch "0.8.0"]]
+                 [me.raynes/conch "0.8.0"]
+                 [expound "0.3.3"]]
   :main ^:skip-aot pho-diff.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/pho_diff/core.clj
+++ src/pho_diff/core.clj
@@ -1,8 +1,9 @@
 (ns pho-diff.core
   (:gen-class)
-  (:require [clojure.spec.alpha :as spec]
-            [me.raynes.conch :as conch]
-            [clojure.java.shell :as shell]))
+  (:require [clojure.java.shell :as shell]
+            [clojure.spec.alpha :as spec]
+            [expound.alpha :as expound]
+            [me.raynes.conch :as conch]))
 
 (defn -main
   "I don't do a whole lot ... yet."
