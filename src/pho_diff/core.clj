7c1291e3e12b16cae0a0aaa6fc0c14734aaf6ba4
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-11-06 10:48:29 -0500

    Add working diff function
    Currently it's verbatim of the shell commands from
    https://stackoverflow.com/a/33673440. I'm looking to make it cleaner or more
    idomatic Clojure.

--- src/pho_diff/core.clj
+++ src/pho_diff/core.clj
@@ -10,13 +10,32 @@
   [& args]
   (println "Hello, World!"))
 
-(def english-cons "resources/inventory")
+(def tagalog-cons "resources/inventory/tagalogipacons.gif")
+(def english-cons "resources/inventory/englishipacons.gif")
 
 
 (conch/with-programs [ls] (ls english-cons))
 
-(conch/programs convert)
+(conch/programs convert cd ls pwd)
 
 (shell/sh "ls" english-cons)
-
 (shell/sh "convert")
+
+(defn diff
+  "Return the diff gif for languages a and b.
+
+  The features only found in language a are colored red, and the features only
+  found in language b are colored green. The features common to both languages
+  remain in grayscale."
+  ([a b out]
+   (convert "(" (str b) "-flatten" "-grayscale" "Rec709Luminance" ")"
+            "(" (str a) "-flatten" "-grayscale" "Rec709Luminance" ")"
+            "(" "-clone" "0-1" "-compose" "darken" "-composite" ")"
+            "-channel" "RGB" "-combine" (str out)))
+  ([a b]
+   (diff a b "diff.gif")))
+
+
+(diff english-cons tagalog-cons)
+
+(comment (set! spec/*explain-out* expound/printer))
