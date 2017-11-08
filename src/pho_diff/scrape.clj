de548bb7b52d48fb0f6c4200a29047147de93d10
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-11-08 12:09:18 -0500

    Add fetch function and example data
    `fetch-url` is from David Nolen's Enlive tutorial:
    https://github.com/swannodette/enlive-tutorial

--- src/pho_diff/scrape.clj
+++ src/pho_diff/scrape.clj
@@ -2,5 +2,15 @@
   (:require [clojure.spec.alpha :as spec]
             [clojure.spec.gen.alpha :as spec.gen]
             [clojure.spec.test.alpha :as spec.test]
+            [clojure.string :as string]
             [expound.alpha :as expound]
             [net.cgrand.enlive-html :as enlive]))
+
+(def archive-url "http://accent.gmu.edu/browse_native.php?function=detail&languageid=38")
+
+(defn fetch-url
+  "Grab the contents of the url specified"
+  [url]
+  (enlive/html-resource (java.net.URL. url)))
+
+(def html-data (fetch-url archive-url))
