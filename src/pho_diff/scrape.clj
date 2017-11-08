98c22affacd3cf4b8643a8098bcc1704aa7c1e03
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-11-08 12:12:54 -0500

    Add language-name function
    Parses the page to get the language name and returns it as a string.

--- src/pho_diff/scrape.clj
+++ src/pho_diff/scrape.clj
@@ -21,3 +21,20 @@
 
 (fetch-gif-urls html-data)
 
+(spec/fdef language-name
+  :args (spec/cat :html-data seq?)
+  :ret string?)
+
+(defn- language-name
+  "Return the string of the language
+
+  TODO: parsing the content is still a bit hardcoded. Idomatic Enlive for this
+  would be nice."
+  [html-data]
+  (-> (enlive/select html-data [:div.content :h5])
+      first
+      :content
+      first
+      (string/replace "Native Phonetic Inventory:" "")
+      string/triml))
+
