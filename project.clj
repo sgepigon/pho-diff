d760423124978c2f94a582fc962fca60d770b85c
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-11-27 12:30:15 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[enlive "1.1.6"]
-                 [expound "0.3.3"]
+                 [expound "0.3.4"]
                  [me.raynes/conch "0.8.0"]
-                 [org.clojure/clojure "1.9.0-beta4"]
-                 [org.clojure/test.check "0.9.0"]]
+                 [org.clojure/clojure "1.9.0-RC1"]
+                 [org.clojure/test.check "0.10.0-alpha2"]]
   :main ^:skip-aot pho-diff.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
