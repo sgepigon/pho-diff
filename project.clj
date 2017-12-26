3f7b663189daf10a7d7cf26e839b16462a9705be
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-12-26 18:33:59 -0500

    Move spec related libaries into dev dependencies
    Also update project description.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject pho-diff "0.1.0-SNAPSHOT"
-  :description "Compare the phonetic inventory of two languages."
+  :description "Visually compare the phonetic inventories of two languages."
   :url "https://github.com/sgepigon/pho-diff"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[enlive "1.1.6"]
-                 [expound "0.4.0"]
                  [me.raynes/conch "0.8.0"]
-                 [org.clojure/clojure "1.9.0"]
-                 [org.clojure/test.check "0.10.0-alpha2"]
-                 [spec-provider "0.4.9"]
-                 [orchestra "2017.11.12-1"]]
+                 [org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies [[expound "0.4.0"]
+                                  [orchestra "2017.11.12-1"]
+                                  [org.clojure/test.check "0.10.0-alpha2"]
+                                  [spec-provider "0.4.9"]]}}
   :main ^:skip-aot pho-diff.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
