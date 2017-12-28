47fa5f2d71d04bfc9149dc0f6c37c33b0d7af485
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-12-28 10:38:12 -0500

    Fix malformed project.clj
    Had duplicate :profiles keys.

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
   :dependencies [[enlive "1.1.6"]
                  [me.raynes/conch "0.8.0"]
                  [org.clojure/clojure "1.9.0"]]
+  :main ^:skip-aot pho-diff.core
+  :target-path "target/%s"
   :profiles {:dev {:dependencies [[expound "0.4.0"]
                                   [orchestra "2017.11.12-1"]
                                   [org.clojure/test.check "0.10.0-alpha2"]
-                                  [spec-provider "0.4.9"]]}}
-  :main ^:skip-aot pho-diff.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+                                  [spec-provider "0.4.9"]]}
+             :uberjar {:aot :all}})
