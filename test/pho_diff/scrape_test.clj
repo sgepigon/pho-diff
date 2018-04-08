b901f9a10f4e460aab844bc5a225ca148173b191
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-04-08 12:56:28 -0400

    Add generative tests

--- test/pho_diff/scrape_test.clj
+++ test/pho_diff/scrape_test.clj
@@ -1,6 +1,9 @@
 (ns pho-diff.scrape-test
-  (:require [clojure.test :refer :all]
-            [pho-diff.scrape :as scrape]))
+  (:require [clojure.spec.gen.alpha :as spec.gen]
+            [clojure.spec.test.alpha :as spec.test]
+            [clojure.test :refer :all]
+            [pho-diff.scrape :as scrape]
+            [user :as user]))
 
 (deftest other-sounds-test
   (testing "English has a non-`nil` other sounds."
@@ -9,4 +12,6 @@
                             "5 diphthongs"}})))
   (testing "\"greek\" returns `nil` (earlier implementations incorrectly
   returned a map)."
-    (is (nil? (scrape/other-sounds "greek")))))
+    (is (nil? (scrape/other-sounds "greek"))))
+  (testing "Generative testing via spec. There are 369 languages."
+    (is (nil? (:false (user/check-result `scrape/other-sounds 369))))))
